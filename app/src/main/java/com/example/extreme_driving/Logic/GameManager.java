2673a2fb2785df0d84f9200b3b90f7c6bd86338c
ShaharZeharia <shacharz2000@gmail.com>
2024-06-24 19:00:00 +0300

    HW1

--- app/src/main/java/com/example/extreme_driving/Logic/GameManager.java
+++ app/src/main/java/com/example/extreme_driving/Logic/GameManager.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class GameManager {
     private int numCollisions = 0;
-    private int life;
+    private final int life;
     private int carCol;
 
-    private int[][] board; //empty is 0,stone is 1 ,car is 2
-    private int rows;
-    private int cols;
-    private Random rnd = new Random();
+    private final int[][] board; //empty is 0,stone is 1
+    private final int rows;
+    private final int cols;
+    private final Random rnd = new Random();
 
     public GameManager() {
         this(3, 5, 3);
@@ -23,8 +23,8 @@ public class GameManager {
         this.cols = cols;
         this.board = new int[rows][cols];
         clearBoard();
-        setInitialCarPosition();
     }
+
     public int getNumCollisions() {
         return numCollisions;
     }
@@ -40,57 +40,64 @@ public class GameManager {
     public int[][] getBoard() {
         return board;
     }
+
     public void moveCarLeft() {
         if (carCol > 0) {
-            board[rows - 1][carCol] = 0;
             carCol--;
-            board[rows - 1][carCol] = 2;
         }
     }
 
     public void moveCarRight() {
         if (carCol < cols - 1) {
-            board[rows - 1][carCol] = 0;
             carCol++;
-            board[rows - 1][carCol] = 2;
         }
     }
+
     public void clearBoard() {
-        for (int i = 0; i < rows ; i++) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 board[i][j] = 0;
             }
         }
     }
-    public void setInitialCarPosition() {
-        board[rows - 1][carCol] = 2;
-    }
+
     public void setRandomRock() {
         int colIndex = rnd.nextInt(cols);
         board[0][colIndex] = 1;
     }
-    public boolean moveRocksAndCheckCollision() {
-        boolean isCollision = false;
-        for (int row = rows - 2; row >= 0; row--) {
+
+    public void moveStones() {
+        for (int row = rows - 1; row >= 0; row--) {
             for (int col = 0; col < cols; col++) {
                 if (board[row][col] == 1) {
                     board[row][col] = 0;
-                    if (row == rows - 2 && col == carCol) {
-                        numCollisions++;
-                        isCollision = true;
+                    if (row < rows - 1) {
+                        board[row + 1][col] = 1;
                     }
-                    board[row + 1][col] = 1;
                 }
             }
         }
+    }
+
+    public boolean checkCollision() {
+        boolean isCollision = false;
+        if (board[rows - 1][carCol] == 1) {
+            numCollisions++;
+            board[rows - 1][carCol] = 0;
+            isCollision = true;
+        }
         return isCollision;
     }
+
     public void resetGame() {
         numCollisions = 0;
+        carCol = 1;
         clearBoard();
-        setInitialCarPosition();
     }
+
     public boolean isGameLost() {
         return life == numCollisions;
     }
 }
+
+
--- app/src/main/java/com/example/extreme_driving/MainActivity.java
+++ app/src/main/java/com/example/extreme_driving/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.extreme_driving;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,30 +12,29 @@ import androidx.appcompat.widget.AppCompatImageView;
 import com.example.extreme_driving.Logic.GameManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
     private static final long DELAY = 1000L;
     private AppCompatImageView[] main_IMG_hearts;
-    private AppCompatImageView[][] main_board;
+    private AppCompatImageView[][] main_IMG_stone;
+    private AppCompatImageView[] main_IMG_racing_car;
     private FloatingActionButton main_BTN_left_arrow;
     private FloatingActionButton main_BTN_right_arrow;
     private GameManager gameManager;
-    private Handler handler = new Handler();
+    private final Handler handler = new Handler();
     private int iterationCounter = 0;
-    private Runnable gameRunnable= new Runnable() {
+    private final Runnable gameRunnable= new Runnable() {
         @Override
         public void run() {
             handler.postDelayed(this, DELAY);
+            gameManager.moveStones();
             if (iterationCounter % 2 == 0) {
                 gameManager.setRandomRock();
             }
-            boolean isCollision = gameManager.moveRocksAndCheckCollision();
+            boolean isCollision = gameManager.checkCollision();
             iterationCounter++;
-            refreshUI(isCollision);
+            updateGameUI(isCollision);
         }
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +42,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViews();
         gameManager = new GameManager(main_IMG_hearts.length,
-                main_board.length,
-                main_board[0].length);
+                main_IMG_stone.length,
+                main_IMG_stone[0].length);
         initViews();
-        startGame();
     }
 
-    private void initViews() {
-        main_board[4][0].setVisibility(View.INVISIBLE);
-        main_board[4][1].setVisibility(View.VISIBLE);
-        main_board[4][2].setVisibility(View.INVISIBLE);
-
-        for (int i = 0; i < main_board.length - 1; i++) {
-            for (int j = 0; j < main_board[0].length; j++) {
-                main_board[i][j].setVisibility(View.INVISIBLE);
-            }
+private void initViews() {
+    for (int i = 0; i < main_IMG_stone.length; i++) {
+        for (int j = 0; j < main_IMG_stone[0].length; j++) {
+            main_IMG_stone[i][j].setVisibility(View.INVISIBLE);
         }
-        main_BTN_left_arrow.setOnClickListener(v -> moveRacingCarLeft());
-        main_BTN_right_arrow.setOnClickListener(v -> moveRacingCarRight());
     }
-
+    updateCarPosition();
+    main_BTN_left_arrow.setOnClickListener(v -> moveRacingCarLeft());
+    main_BTN_right_arrow.setOnClickListener(v -> moveRacingCarRight());
+}
 
     private void findViews() {
         main_IMG_hearts = new AppCompatImageView[]{
@@ -72,23 +65,28 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.main_IMG_heart3)
         };
 
-        main_board = new AppCompatImageView[5][3];
-        main_board[0][0] = findViewById(R.id.stone_0_0);
-        main_board[0][1] = findViewById(R.id.stone_0_1);
-        main_board[0][2] = findViewById(R.id.stone_0_2);
-        main_board[1][0] = findViewById(R.id.stone_1_0);
-        main_board[1][1] = findViewById(R.id.stone_1_1);
-        main_board[1][2] = findViewById(R.id.stone_1_2);
-        main_board[2][0] = findViewById(R.id.stone_2_0);
-        main_board[2][1] = findViewById(R.id.stone_2_1);
-        main_board[2][2] = findViewById(R.id.stone_2_2);
-        main_board[3][0] = findViewById(R.id.stone_3_0);
-        main_board[3][1] = findViewById(R.id.stone_3_1);
-        main_board[3][2] = findViewById(R.id.stone_3_2);
+        main_IMG_racing_car = new AppCompatImageView[]{
+                findViewById(R.id.racing_car_4_0),
+                findViewById(R.id.racing_car_4_1),
+                findViewById(R.id.racing_car_4_2)
+        };
 
-        main_board[4][0] = findViewById(R.id.racing_car_4_0);
-        main_board[4][1] = findViewById(R.id.racing_car_4_1);
-        main_board[4][2] = findViewById(R.id.racing_car_4_2);
+        main_IMG_stone = new AppCompatImageView[5][3];
+        main_IMG_stone[0][0] = findViewById(R.id.stone_0_0);
+        main_IMG_stone[0][1] = findViewById(R.id.stone_0_1);
+        main_IMG_stone[0][2] = findViewById(R.id.stone_0_2);
+        main_IMG_stone[1][0] = findViewById(R.id.stone_1_0);
+        main_IMG_stone[1][1] = findViewById(R.id.stone_1_1);
+        main_IMG_stone[1][2] = findViewById(R.id.stone_1_2);
+        main_IMG_stone[2][0] = findViewById(R.id.stone_2_0);
+        main_IMG_stone[2][1] = findViewById(R.id.stone_2_1);
+        main_IMG_stone[2][2] = findViewById(R.id.stone_2_2);
+        main_IMG_stone[3][0] = findViewById(R.id.stone_3_0);
+        main_IMG_stone[3][1] = findViewById(R.id.stone_3_1);
+        main_IMG_stone[3][2] = findViewById(R.id.stone_3_2);
+        main_IMG_stone[4][0] = findViewById(R.id.stone_4_0);
+        main_IMG_stone[4][1] = findViewById(R.id.stone_4_1);
+        main_IMG_stone[4][2] = findViewById(R.id.stone_4_2);
 
         main_BTN_left_arrow = findViewById(R.id.main_BTN_left_arrow);
         main_BTN_right_arrow = findViewById(R.id.main_BTN_right_arrow);
@@ -102,48 +100,55 @@ public class MainActivity extends AppCompatActivity {
     private void moveRacingCarRight() {
         gameManager.moveCarRight();
         updateCarPosition();
-        //updateBoard();
     }
 
     private void updateCarPosition() {
-        int racingCarCol = gameManager.getCarCol();
-        main_board[4][0].setVisibility(racingCarCol == 0 ? View.VISIBLE : View.INVISIBLE);
-        main_board[4][1].setVisibility(racingCarCol == 1 ? View.VISIBLE : View.INVISIBLE);
-        main_board[4][2].setVisibility(racingCarCol == 2 ? View.VISIBLE : View.INVISIBLE);
+        int carCol = gameManager.getCarCol();
+        boolean isCollision = gameManager.checkCollision();
+        if (isCollision){
+            main_IMG_stone[main_IMG_stone.length - 1][carCol].setVisibility(View.INVISIBLE);
+            handleCollisionUI();
+        }
+        for (int i = 0; i < main_IMG_racing_car.length; i++) {
+            main_IMG_racing_car[i].setVisibility(i == carCol ? View.VISIBLE : View.INVISIBLE);
+        }
     }
 
     private void updateBoard() {
         int[][] board = gameManager.getBoard();
-        for (int i = 0; i < board.length - 1; i++) {
+        for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 1) {
-                    main_board[i][j].setVisibility(View.VISIBLE);
-                } else if (board[i][j] == 2) {
-                    main_board[i][j].setVisibility(View.VISIBLE);
-                } else {
-                    main_board[i][j].setVisibility(View.INVISIBLE);
+                    main_IMG_stone[i][j].setVisibility(View.VISIBLE);
+                }  else {
+                    main_IMG_stone[i][j].setVisibility(View.INVISIBLE);
                 }
             }
         }
+        updateCarPosition();
     }
 
-    public void refreshUI(boolean isCollision) {
-        updateBoard();
-        //updateHearts();
-        if (isCollision) {
-            main_IMG_hearts[gameManager.getNumCollisions() - 1]
-                    .setVisibility(View.INVISIBLE);
-            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-            vibrator.vibrate(500);
-            Toast.makeText(this, "Crash occurred!", Toast.LENGTH_SHORT).show();
 
-            if (gameManager.isGameLost()) {
-                //Toast.makeText(this, "Game Over!", Toast.LENGTH_LONG).show();
-                gameManager.resetGame();
-                updateHearts();
-            }
+    public void updateGameUI(boolean isCollision) {
+        updateBoard();
+        if (isCollision) {
+            handleCollisionUI();
         }
     }
+
+    public void handleCollisionUI() {
+        main_IMG_hearts[gameManager.getNumCollisions() - 1]
+                .setVisibility(View.INVISIBLE);
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(500);
+        Toast.makeText(this, "Crash occurred!", Toast.LENGTH_SHORT).show();
+
+        if (gameManager.isGameLost()) {
+            gameManager.resetGame();
+            updateHearts();
+        }
+    }
+
 
     public void startGame() {
         handler.postDelayed(gameRunnable, 0);
@@ -162,9 +167,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         stopGame();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startGame();
     }
 }
 
